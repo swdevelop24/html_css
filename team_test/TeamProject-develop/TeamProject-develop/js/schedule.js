c10fada54584b529826a2c0f20ffcf85807ac1bd
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2025-01-19 19:32:51 -0600

    Delete team_test/TeamProject-develop/TeamProject-develop directory

--- team_test/TeamProject-develop/TeamProject-develop/js/schedule.js
+++
@@ -1,9 +0,0 @@
-function scrollLeftHandler(day) {
-  var gameInfo = document.getElementById('game-info-container-' + day);
-  gameInfo.scrollLeft -= 200; 
-}
-
-function scrollRightHandler(day) {
-  var gameInfo = document.getElementById('game-info-container-' + day);
-  gameInfo.scrollLeft += 200; 
-}
